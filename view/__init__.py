0256ce0ec9386360c51d62cbf9cdc3cfdeff17c3
javohir <javohirbaxridinov77@gmail.com>
2023-06-30 16:37:27 +0700

    у меня 7 файлов

---
+++ view/__init__.py
@@ -0,0 +1,3 @@
+from .console import menu, show_contacts, print_message, input_contact, input_return
+from .text import *
+__al__ = ['menu', 'show_contacts', 'print_massage', 'input_contacts', 'input_return']
